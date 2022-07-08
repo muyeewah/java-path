d08f3ec761e931fabe10a66f00b8213f9fabd141
fobabire <fobabire@ecobank.com>
2022-07-08 19:24:38 +0100

    Inclusion of employee class

---
+++ src/com/sbolaji/javabasics/Employee.java
@@ -0,0 +1,9 @@
+package com.sbolaji.javabasics;
+
+public class Employee {
+
+    private int employeeID;
+    String employeeName;
+    private int employeeAge;
+
+}
--- src/com/sbolaji/javabasics/JavaBasics.java
+++ src/com/sbolaji/javabasics/JavaBasics.java
@@ -1,7 +1,15 @@
 package com.sbolaji.javabasics;
 
+import java.text.MessageFormat;
+
 public class JavaBasics {
     public static void main(String[] args) {
-        System.out.println("Hello Mi Brethren");
+
+        Employee employee;
+        employee = new Employee();
+
+        employee.employeeName = "Stanley";
+        System.out.println(MessageFormat.format("Employee is {0}", employee.employeeName));
+
     }
 }
