949f19138c5b717231953d22374800af3fc92f9a
muyeewah <muyewahqomeyour@gmail.com>
2022-07-11 01:41:18 +0100

    Two dimensional arrays

--- src/com/sbolaji/javabasics/ArraysAndCollections.java
+++ src/com/sbolaji/javabasics/ArraysAndCollections.java
@@ -10,10 +10,25 @@ public class ArraysAndCollections {
         secondEmployee = new Employee(12, "Jones", 22);
         Employee[] listOfEmployees = {firstEmployee, secondEmployee};
         try {
+            System.out.println("Array length is: " + listOfEmployees.length);
             System.out.println(MessageFormat.format("2nd employee is: {0}", listOfEmployees[1].getEmployeeName()));
         }
         catch (Exception exceptionMessage) {
             System.out.println(exceptionMessage.getMessage());
+        }
+    }
+}
+
+class TwoDimensionalArrays {
+    public static void main(String[] args) {
+
+        String[][] cars = new String[2][3];
+        System.out.println("Length of cars array is: " + cars.length);
+
+        for (int row = 0; row < cars.length; row+=1) {
+            System.out.println();
+            for (int column = 0; column < cars[row].length; column+=1)
+                System.out.print(MessageFormat.format("Car at row[{0}],column[{1}] | ", row, column));
         }
     }
 }
